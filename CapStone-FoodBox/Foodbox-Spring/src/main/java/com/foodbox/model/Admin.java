47f2cff24b00609b57c590e7116cc33d58783a6a
mayur <mayursolanki66@gmail.com>
2022-03-05 01:17:47 +0530

    first commit

---
+++ CapStone-FoodBox/Foodbox-Spring/src/main/java/com/foodbox/model/Admin.java
@@ -0,0 +1,42 @@
+package com.foodbox.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Admin {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long userId;
+	private String username;
+	private String password;
+
+	public Admin() {
+		super();
+	}
+
+	public Admin(String username, String password) {
+		super();
+		this.username = username;
+		this.password = password;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
